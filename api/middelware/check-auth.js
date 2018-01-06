3cfe0806e31282e15ad98c0c424760fd68503752
selimgnaoui <selim.gnaoui@gmail.com>
2018-01-06 04:20:44 +0100

    Update check-auth.js

--- api/middelware/check-auth.js
+++ api/middelware/check-auth.js
@@ -12,7 +12,7 @@ module.exports= (req,res,next)=> {
     catch (error)
     { return res.status(404).json({
             
-            message :" Authentification Failed"
+            message :" Authentification Failed "
     });
         
     }
